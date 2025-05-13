794b25a1a5cb361e3b7fb9544e2da8e9cc69a607
JulianRosales07 <142527675+JulianRosales07@users.noreply.github.com>
2025-05-13 16:56:02 -0500

    cambios

--- microPiezas/src/models/piezasModel.js
+++ microPiezas/src/models/piezasModel.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root', 
-    password: '1193051330', 
-    database: 'taller_mecanico'
+    host: 'interchange.proxy.rlwy.net:39809',
+    user: 'root', // Reemplaza con tu usuario
+    password: 'klGJkEKVAWycQdDBYJZBQZtBPRTWgZNa', // Reemplaza con tu contraseña
+    database: 'railway'
 });
 
 async function traerPiezas() {
